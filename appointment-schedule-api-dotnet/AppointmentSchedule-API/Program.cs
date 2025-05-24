397b2a3eff1eb4e08a873a1b57dd59b0e2d752b5
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-05-24 20:10:51 -0300

    Ajuste Swagger

--- appointment-schedule-api-dotnet/AppointmentSchedule-API/Program.cs
+++ appointment-schedule-api-dotnet/AppointmentSchedule-API/Program.cs
@@ -23,11 +23,9 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
