4795dde84dffdba4eb38da4bd80a8d57e288058c
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-05-24 21:34:58 -0300

    Ajustando Caminho Banco

--- appointment-schedule-api-dotnet/AppointmentSchedule-API/Configurations/DatabaseConfig.cs
+++ appointment-schedule-api-dotnet/AppointmentSchedule-API/Configurations/DatabaseConfig.cs
@@ -1,11 +1,16 @@
 ﻿using AppointmentSchedule_Infra.Context;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 public static class DatabaseConfig
 {
     public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        // Caminho físico absoluto do arquivo do banco
+        var relativePath = configuration.GetConnectionString("DefaultConnection");
+
+        var dbPath = Path.Combine(AppContext.BaseDirectory, relativePath.Replace("Data Source=", "").Replace("\\", Path.DirectorySeparatorChar.ToString()));
+        var connectionString = $"Data Source={dbPath}";
 
         services.AddDbContext<AppointmentScheduleContext>(options =>
             options.UseSqlite(connectionString)
